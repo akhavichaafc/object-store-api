7836c5f2c3efcf03d07f30eaee334f112d9544af
Shemy Gan <xuemei.gan@canada.ca>
2019-09-23 13:05:57 -0400

    Feature #17270 Multipart Upload Endpoint,added test application yml file to fix the no host to route.

--- src/test/java/ca/gc/aafc/objectstore/api/fileupload/FileUploadIT.java
+++ src/test/java/ca/gc/aafc/objectstore/api/fileupload/FileUploadIT.java
@@ -9,31 +9,29 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.context.WebApplicationContext;
 
+@ActiveProfiles("test")
 @SpringBootTest
 public class FileUploadIT {
 
   @Autowired
   protected WebApplicationContext wac;
-
+  
   @Test
   public void fileUpload_onMultipartRequest_acceptFile() throws Exception {
 
     MockMultipartFile file = new MockMultipartFile("file", "testfile", null,
         "Test Content".getBytes());
-
-    MultiValueMap<String, Object> map = new LinkedMultiValueMap<String, Object>();
-    map.add("file", file);
-
+    
     webAppContextSetup(this.wac).build()
         .perform(MockMvcRequestBuilders.multipart("/api/v1/file").file(file)
-        .param("bucket", "testbucket"))
+        .param("bucket", "testbucket"+ Math.random()))
         .andExpect(status().is(200))
         .andExpect(content().string(containsString("testfile")));
-  }
+    
+ }
 
 }
