bfbb761e40768dfa805f560b35d335f76c026557
Christian Gendreau <christiangendreau@gmail.com>
2019-10-24 16:49:00 -0400

    Support #17672 Add support for adding managed attributes to metadata Cleanup test

--- src/test/java/ca/gc/aafc/objectstore/api/rest/ObjectStoreMetadataJsonApiIT.java
+++ src/test/java/ca/gc/aafc/objectstore/api/rest/ObjectStoreMetadataJsonApiIT.java
@@ -2,15 +2,10 @@ package ca.gc.aafc.objectstore.api.rest;
 
 import java.time.OffsetDateTime;
 import java.util.Map;
-import java.util.UUID;
-
-import org.junit.jupiter.api.BeforeEach;
 
 import ca.gc.aafc.objectstore.api.dto.ObjectStoreMetadataDto;
-import ca.gc.aafc.objectstore.api.entities.ManagedAttribute;
 import ca.gc.aafc.objectstore.api.entities.ObjectStoreMetadata;
 import ca.gc.aafc.objectstore.api.mapper.ObjectStoreMetadataMapper;
-import ca.gc.aafc.objectstore.api.testsupport.factories.ManagedAttributeFactory;
 import ca.gc.aafc.objectstore.api.testsupport.factories.ObjectStoreMetadataFactory;
 
 public class ObjectStoreMetadataJsonApiIT extends BaseJsonApiIntegrationTest {
@@ -18,16 +13,6 @@ public class ObjectStoreMetadataJsonApiIT extends BaseJsonApiIntegrationTest {
   private ObjectStoreMetadataMapper mapper = ObjectStoreMetadataMapper.INSTANCE;
   
   private ObjectStoreMetadata objectStoreMetadata;
-  
-  private UUID managedAttributeId = UUID.randomUUID();
-  
-  @BeforeEach
-  public void setup() {
-    ManagedAttribute ma = ManagedAttributeFactory.newManagedAttribute()
-        .uuid(managedAttributeId).build();
-    // we need to run the setup in another transaction and commit it otherwise it can't be visible to the test web server.
-    runInNewTransaction( em -> em.persist(ma));
-  }
   
   @Override
   protected String getResourceUnderTest() {
@@ -70,17 +55,6 @@ public class ObjectStoreMetadataJsonApiIT extends BaseJsonApiIntegrationTest {
     ObjectStoreMetadataDto objectStoreMetadatadto = mapper.toDto(objectStoreMetadata);
     
     return toAttributeMap(objectStoreMetadatadto);
-  }
-  
-  @Override
-  protected Map<String, Object> buildRelationshipMap() {
-/*    ImmutableMap.Builder<String, Object> relationships = new ImmutableMap.Builder<>();
-    relationships.put("type", "managed-attribute").put("id", managedAttributeId.toString()).build();
-
-    ImmutableMap.Builder<String, Object> bldr = new ImmutableMap.Builder<>();
-    bldr.put("data", relationships.build());
-    return ImmutableMap.of("managedAttribute", bldr.build());*/
-    return null;
   }
   
 }
