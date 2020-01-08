0b2c4153652b791da906182ef071004cb1f2c18f
Dooley <dooleyp@onottra670238x>
2020-01-07 20:47:14 -0500

    aa

--- env_proj.sh
+++ env_proj.sh
@@ -7,11 +7,11 @@ export BIC_PORT=8081
 export BIC_USER="user"
 #printenv BIC_USER
 
-export SPRING_DATASOURCE_URL='jdbc:postgresql://database:5432/object_store'
+#export SPRING_DATASOURCE_URL='jdbc:postgresql://database:5432/object_store'
 printenv SPRING_DATASOURCE_URL
 
-export SPRING_DATASOURCE_USERNAME='postgres'
+#export SPRING_DATASOURCE_USERNAME='postgres'
 printenv SPRING_DATASOURCE_USERNAME
 
-export SPRING_DATASOURCE_PASSWORD='postgres'
+#export SPRING_DATASOURCE_PASSWORD='postgres'
 printenv SPRING_DATASOURCE_PASSWORD
