c6488ce802fb805f01c8a2f5cd0a4297052ba34a
pdooley <Francis01!>
2020-01-23 16:23:49 -0500

    Object_store_api is now building and connecting to postgres.  Doesn't seem to be populating database.

--- env_proj.sh
+++ env_proj.sh
@@ -10,21 +10,6 @@ export BIC_PORT_EXT=8081
 export BIC_USER="user"
 #printenv BIC_USER
 
-export SPRING_DATASOURCE_URL='jdbc:postgresql://database:5532/seqdb_obj?currentSchema=seqdb'
-printenv SPRING_DATASOURCE_URL
-
-export SPRING_DATASOURCE_USERNAME='seqdb_webapp'
-printenv SPRING_DATASOURCE_USERNAME
-
-export SPRING_DATASOURCE_PASSWORD='changeme'
-printenv SPRING_DATASOURCE_PASSWORD
-
-export SPRING_LIQUIBASE_USER='seqdb_migration'
-printenv SPRING_LIQUIBASE_USER
-
-export SPRING_LIQUIBASE_PASSWORD='changeme2'
-printenv SPRING_LIQUIBASE_PASSWORD
-
 export IMPORT_SAMPLE_ACCOUNTS=true
 printenv IMPORT_SAMPLE_ACCOUNTS
 
