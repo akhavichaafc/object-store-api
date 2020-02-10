2a139b8a287215a2e946a5a5c2454e5fbf8f206a
Jonathan keyuk <jonathan.keyuk@canada.ca>
2020-02-10 08:42:38 -0500

    Feature #18631 Change acDerivedFrom to relationship
    https://redmine.biodiversity.agr.gc.ca/issues/18631
    
    Add create_ValidResource_ResourcePersisted test to ObjectStoreMetadataRepositoryCRUDIT

--- src/test/java/ca/gc/aafc/objecstore/api/repository/ObjectStoreMetadataRepositoryCRUDIT.java
+++ src/test/java/ca/gc/aafc/objecstore/api/repository/ObjectStoreMetadataRepositoryCRUDIT.java
@@ -2,14 +2,17 @@ package ca.gc.aafc.objecstore.api.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.Collections;
+import java.util.UUID;
 
 import javax.inject.Inject;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import ca.gc.aafc.objectstore.api.TestConfiguration;
 import ca.gc.aafc.objectstore.api.dto.ObjectStoreMetadataDto;
 import ca.gc.aafc.objectstore.api.entities.ObjectStoreMetadata;
 import ca.gc.aafc.objectstore.api.respository.ObjectStoreResourceRepository;
@@ -59,5 +62,24 @@ public class ObjectStoreMetadataRepositoryCRUDIT extends BaseRepositoryTest {
     // fetches relations marked with "LookupIncludeBehavior.AUTOMATICALLY_ALWAYS" outside of "findAll".
     // The test for this inclusion are done in MetadataToManagedAttributeMapRepositoryCRUDIT.
   }
-    
+
+  @Test
+  public void create_ValidResource_ResourcePersisted() {
+    ObjectStoreMetadataDto derived = new ObjectStoreMetadataDto();
+    derived.setUuid(testObjectStoreMetadata.getUuid());
+
+    UUID dtoUuid = UUID.randomUUID();
+    ObjectStoreMetadataDto dto = new ObjectStoreMetadataDto();
+    dto.setUuid(dtoUuid);
+    dto.setBucket(TestConfiguration.TEST_BUCKET);
+    dto.setFileIdentifier(TestConfiguration.TEST_FILE_IDENTIFIER);
+    dto.setAcDerivedFrom(derived);
+
+    objectStoreResourceRepository.create(dto);
+
+    ObjectStoreMetadata result = findUnique(ObjectStoreMetadata.class, "uuid", dtoUuid);
+    assertEquals(dtoUuid, result.getUuid());
+    assertEquals(derived.getUuid(), result.getAcDerivedFrom().getUuid());
+  }
+
 }
