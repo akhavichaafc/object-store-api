84743a8b2b22d223e7465557a9f132b931736c90
Jonathan keyuk <jonathan.keyuk@canada.ca>
2020-02-10 08:50:19 -0500

    Feature #18631 Change acDerivedFrom to relationship
    https://redmine.biodiversity.agr.gc.ca/issues/18631
    
    Remove unused import

--- src/test/java/ca/gc/aafc/objecstore/api/repository/ObjectStoreMetadataRepositoryCRUDIT.java
+++ src/test/java/ca/gc/aafc/objecstore/api/repository/ObjectStoreMetadataRepositoryCRUDIT.java
@@ -2,7 +2,6 @@ package ca.gc.aafc.objecstore.api.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.Collections;
 import java.util.UUID;
