daab89da9114dcec4a6511413149747873817e6a
Alex Khavich <alex.khavich@canada.ca>
2020-03-23 13:55:02 -0400

    In-progress: Feature #18793 - implement keycloak login
    https://redmine.biodiversity.agr.gc.ca/issues/18793
    
    - reconfigured Docker network setup so both browser redirection and
    back-channel communication work

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -10,4 +10,6 @@ VERSION=$(cat ./pom.xml | grep -m 1 '<version>' | awk -F"[><]" '{print $3}')
 export VERSION
 echo "Version: '$VERSION'"
 echo "executing java"
-exec java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -jar $1-$VERSION.jar
+# enable remote debugging
+exec java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8000 -jar $1-$VERSION.jar
+# exec java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -jar $1-$VERSION.jar
