e8856c4bb8c1b721436b49b7a4a2f61004eb4177
Christian Gendreau <cgendreau@users.noreply.github.com>
2020-04-17 12:58:51 -0400

    Formating and warning when KC disabled

--- src/main/java/ca/gc/aafc/objectstore/api/security/keycloak/KeycloakDisabledAuthConfig.java
+++ src/main/java/ca/gc/aafc/objectstore/api/security/keycloak/KeycloakDisabledAuthConfig.java
@@ -11,23 +11,22 @@ import lombok.extern.log4j.Log4j2;
 @Configuration
 @ConditionalOnProperty(value = "keycloak.enabled", havingValue = "false")
 @Log4j2
+
 /**
  * A dummy security configuration file in order to prevent Spring Boot from 
  * automatically configuring its own security when Keycloak is disabled.
  * In production, Keycloak should always be enabled.
- * @author Alex Khavich
  */
 public class KeycloakDisabledAuthConfig extends WebSecurityConfigurerAdapter {
   
   public KeycloakDisabledAuthConfig() {
     super();
-    log.info("KeycloakDisabledAuthConfig created");
+    log.warn("Keycloak DISABLED. KeycloakDisabledAuthConfig created");
   }
   
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable();
-    
     http.authorizeRequests().antMatchers("/**").permitAll();
   }
   
